949d903a69a26434e617356c5f19e4de91110355
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2024-11-28 18:12:17 +0900

    add

--- test.bash
+++ test.bash
@@ -7,17 +7,17 @@ ng () {
 
 res=0
 
-#out=$(seq 5 | ./plus)
-#[ "${out}" = 15 ] || ng "$LINENO"
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
 
 ### hennanov###
-#out=$(echo a | ./plus)
-#[ "$?" = 1 ] || ng "$LINENO"
-#[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo a | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
-#out=$(echo | ./plus)
-#[ "$?" = 1 ] || ng "$LINENO"
-#[ "${out}" = "" ] || ng "$LINENO"
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "${out}" = "" ] || ng "$LINENO"
 
 [ "${res}" = 0 ] && echo OK
 
