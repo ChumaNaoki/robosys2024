20f0e4d745c040e2b1b17b11ab3e42bd4f2843dc
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2024-11-29 01:02:17 +0900

    onkai

--- test.bash
+++ test.bash
@@ -19,6 +19,12 @@ out=$(echo | ./plus)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
+out=$(./onkai 300)
+[ "${out}" = "入力された周波数300.0Hzは、
+ピアノ鍵盤だと42番目の音階です。
+音階名だとD4(レ4)となり、
+293.66Hz~311.13Hzの音階に当たります。" ] || ng "$LINENO"
+
 [ "${res}" = 0 ] && echo OK
 
 exit $res
