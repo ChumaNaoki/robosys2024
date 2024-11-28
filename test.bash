2c758e57ff5897cea8c090ae4967db9ac5ddcec5
Chuma Naoki <s23C1091UY@s.chibakoudai.jp>
2024-11-28 22:06:11 +0900

    add

--- test.bash
+++ test.bash
@@ -19,6 +19,16 @@ out=$(echo | ./plus)
 [ "$?" = 1 ] || ng "$LINENO"
 [ "${out}" = "" ] || ng "$LINENO"
 
+out=$(./attimuitehoi)
+out=3
+if num == 1
+[ "${out}" = "私はグーを出したよ！" ] || ng "$LINENO"
+elif num == 2
+[ "${out}" = "私はチョキを出したよ！" ] || ng "$LINENO"
+elif num == 3
+[ "${out}" = "私はパーを出したよ！" ] || ng "$LINENO"
+
+
 [ "${res}" = 0 ] && echo OK
 
 exit $res
